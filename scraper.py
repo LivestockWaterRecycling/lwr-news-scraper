c6fa6bb2a713783090827b86629903bbfd3fabd4
osc-jaimes <oscjaims@gmail.com>
2020-05-29 11:30:14 -0600

    -

--- scraper.py
+++ scraper.py
@@ -17,17 +17,17 @@ class LWRSpider(scrapy.Spider):
     ###
     def parse(self, response):
         #extension for each 'news article' in HTML source
-        SET_SELECTOR = '.uk-panel-title'
+        ARTICLE_SELECTOR = '.uk-panel-title'
 
         #Iterate through every article found
-        for brickset in response.css(SET_SELECTOR):
+        for article in response.css(ARTICLE_SELECTOR):
             TITLE_SELECTOR = 'a ::text'
 
-            #A lot of fast facts - excluding them for now... 
+            #A lot of fast facts - excluding them for now... TODO
             if not 'FAST FACTS' in str(brickset.css(NAME_SELECTOR).extract_first()) :   
                 yield {
                     #Title field will be the first instance of TITLE_SELECTOR in each article
-                    'title': brickset.css(TITLE_SELECTOR).extract_first(),
+                    'title': article.css(TITLE_SELECTOR).extract_first(),
                 }
 
         
