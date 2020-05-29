9a3092933dad116ead8cc023bec883068923f679
osc-jaimes <oscjaims@gmail.com>
2020-05-29 11:49:51 -0600

    adding more fields to results

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 ###
 # This program uses scrapy to 'crawl' or 'spider' through the news page of the LWR Website.
+# It will crawl two specific pages in the LWR website - newsroom/press and newsroom/featured
 #
 # Author: Oscar Jaimes
 # Date: 29/05/2020
@@ -9,26 +10,34 @@ import scrapy
 
 class LWRSpider(scrapy.Spider):
     name = "lwr_spider"
-    start_urls = ['https://www.livestockwaterrecycling.com/newsroom/featured.html']
+    start_urls = ['https://www.livestockwaterrecycling.com/newsroom/press.html']
 
     ###
     # Function that parses a websites HTML source
     # @param response - HTML response
     ###
     def parse(self, response):
-        #extension for each 'news article' in HTML source
+        # extension for each 'news article' in HTML source
         ARTICLE_SELECTOR = '.uk-panel-title'
+        CONTENT_SELECTOR = '.uk-margin'
 
-        #Iterate through every article found
-        for article in response.css(ARTICLE_SELECTOR):
+        # parallel arrays of articles and content
+        articleList = response.css(ARTICLE_SELECTOR)
+        contentList = response.css(CONTENT_SELECTOR)
+
+        # Iterate through every article found
+        for articleIndx in range(len(articleList)):
+
+            # CSS extension for title in each article
             TITLE_SELECTOR = 'a ::text'
-
-            #A lot of fast facts - excluding them for now... TODO
-            if not 'FAST FACTS' in str(brickset.css(NAME_SELECTOR).extract_first()) :   
-                yield {
-                    #Title field will be the first instance of TITLE_SELECTOR in each article
-                    'title': article.css(TITLE_SELECTOR).extract_first(),
-                }
+            DESCRIPTION_SELECTOR = 'p ::text'
+            
+            yield {
+                # Title field will be the first instance of TITLE_SELECTOR in each article
+                'title': articleList[articleIndx].css(TITLE_SELECTOR).extract_first(),
+                'content': contentList[articleIndx].css(DESCRIPTION_SELECTOR).extract_first(),
+                
+            }
 
         
 
