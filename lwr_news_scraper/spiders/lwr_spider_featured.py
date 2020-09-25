9d15b9387e983e465af3f3896eeee63e5cabe96c
osc-jaimes <ojaimes@ualberta.ca>
2020-09-25 12:12:58 -0600

    only need to run python script to scrape. no more cmd line args

--- lwr_news_scraper/spiders/lwr_spider_featured.py
+++ lwr_news_scraper/spiders/lwr_spider_featured.py
@@ -6,26 +6,26 @@ Date: 29/05/2020
 '''
 
 import scrapy
+from scrapy.crawler import CrawlerProcess
+
 
 class LWRSpiderFeatured(scrapy.Spider):
-    
+
     name = "lwr_spider_featured"
 
     # urls that this spider crawls through. Crawls through 1 URL at a time.
-    start_urls = ['https://www.livestockwaterrecycling.com/newsroom/featured.html' ]
-        
+    start_urls = [
+        'https://www.livestockwaterrecycling.com/newsroom/featured.html']
 
     custom_settings = {
         'FEED_URI': '../../featured.json',
     }
-    
 
-
-    
     '''
     Function that parses a websites HTML source
     @param response - HTML response
     '''
+
     def parse(self, response):
 
         open('../../featured.json', 'w').close()
@@ -39,28 +39,26 @@ class LWRSpiderFeatured(scrapy.Spider):
         titleList = response.css(ARTICLE_SELECTOR)
         contentList = response.css(CONTENT_SELECTOR)
         imageList = response.css(IMAGE_SELECTOR)
-        
+
         # Iterate through every article found
         for articleIndx in range(len(titleList)):
 
             # Extensions for each field:
             # CSS:
             TITLE_EXTENSION = 'a ::text'
-            
-            #FAST FACTS are formatted slightly differently, get proper description extension based on title. 
+
+            # FAST FACTS are formatted slightly differently, get proper description extension based on title.
             if 'Lisa' not in titleList[articleIndx].css(TITLE_EXTENSION).extract_first():
                 DESCRIPTION_EXTENSION = 'p ::text'
             else:
                 DESCRIPTION_EXTENSION = 'ul'
-            
+
             # xPath
             LINK_EXTENSION = 'a/@href'
             IMAGE_EXTENSION = 'img/@src'
-            
+
             # Will append to end of this string when constructing image link.
             IMAGE_URL_START = 'https://www.livestockwaterrecycling.com/'
-
-            
 
             # resulting dictionary/map of each article object
             yield {
@@ -70,4 +68,8 @@ class LWRSpiderFeatured(scrapy.Spider):
                 'image': IMAGE_URL_START + imageList[articleIndx].xpath(IMAGE_EXTENSION).extract_first(),
             }
 
-        
+
+if __name__ == "__main__":
+  process = CrawlerProcess()
+  process.crawl(LWRSpiderFeatured)
+  process.start()
